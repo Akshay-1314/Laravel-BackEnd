dfaad4a7907816ea071518329ba98a491a43899c
Akshay-1314 <66550751+Akshay-1314@users.noreply.github.com>
2020-06-10 23:41:29 +0530

    Update Landing_Page.blade.php

--- resources/views/Landing_Page.blade.php
+++ resources/views/Landing_Page.blade.php
@@ -38,6 +38,9 @@
               <li class="nav-item">
                 <a class="nav-link" href="#contact">Contact Us</a>
               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="admin">Booking List</a>
+              </li>
             </ul>
           </div>
         </nav>
