f5ad746ffb6c58e4898e5ffe45a8a3f7701b41e6
Akshay-1314 <66550751+Akshay-1314@users.noreply.github.com>
2020-06-14 12:23:20 +0530

    Add files via upload

---
+++ app/Http/Middleware/RestrictAccess.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RestrictAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(!session()->has('details')){
+            return redirect('/')->with('access','Denied!');
+        }
+        return $next($request);
+    }
+}
