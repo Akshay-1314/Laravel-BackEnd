fa8b638986641263099a5c43be7309479b62b707
Akshay-1314 <66550751+Akshay-1314@users.noreply.github.com>
2020-06-17 23:33:10 +0530

    Update New_Booking_Page.blade.php

--- resources/views/New_Booking_Page.blade.php
+++ resources/views/New_Booking_Page.blade.php
@@ -12,7 +12,25 @@
       <link rel = "icon" href =  
         "https://pbs.twimg.com/profile_images/849341342224351238/cuaVqp5x_400x400.jpg"
         type="image/x-icon">
-      <link rel="stylesheet" href="{{asset('css/style1.css')}}">
+      <style>
+            body{
+              background-color:#161748;
+            }
+            #logo {
+              position:relative;
+                    left:20px;
+                    width:100px;
+            }
+            .sample{
+              text-align:center;
+            }
+            #img3,#img4{
+                width:50px;
+            }
+            #footer{
+              background: #f6f6f9;
+            }
+      </style>
     </head>
     <body>
       <header class="bg-dark h-20 container-fluid">
@@ -32,18 +50,49 @@
           </div>
         </nav>
       </header>
+      @if(session('message'))
+      <div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+          Please click on continue to proceed further
+      </div>
+      @endif
+      
       <section class="container-fluid bg-warning">
         <div>
+          <form action="submit6" method="POST">
+          @csrf
+            <div class="form3 form-group bg-secondary text-white mt-4">
+                  <label for="sel2">Select a Lab</label>
+                  <select class="form-control" id="sel2" name="sellist2">
+                  <option value="" selected>None</option>
+                  @foreach($associations as $value)
+                    <option value="{{$value->lab_name}}" {{old('sellist2')=="$value->lab_name" ? 'selected' : ''}} @if(session('sample')) {{Session::get('sample')==$value->lab_id ? 'selected': ''}} @endif>{{$value->lab_name}}</option>
+                  @endforeach
+                  </select>
+
+                  @error('sellist2')
+                  <div class="text-warning">{{$message}}</div>
+                  @enderror
+            </div>
+            <div class="text-center">
+              <button class="btn bg-secondary text-white" type="submit">Continue</button>
+            </div>
+          </form>
           <form action="submit" method="POST" enctype="multipart/form-data" onsubmit="return validate()">
             @csrf
             <div class="form-group bg-secondary text-white mt-4">
               <label for="sel1">Select a Test/s (hold ctrl to select more than one):</label>
               <select class="form-control" id="sel1" name="sellist1[]" size="5" multiple>
-                <option value="liver function test">Liver Function Test</option>
-                <option value="endoscopy">Endoscopy</option>
-                <option value="mri">MRI</option>
-                <option value="ct">CT</option>
-                <option value="cbp">CBP</option>
+                @if(session('check'))
+                  @foreach(Session::get('check') as $value)
+                  <option value="{{$value->test_name}}" {{ (is_array(old('sellist1')) and in_array("$value->test_name", old('sellist1'))) ? 'selected' : ''}}>{{$value->test_name}}</option>
+                  @endforeach
+                @else
+                  @foreach($tests as $value)
+                    <option value="{{$value->test_name}}" {{ (is_array(old('sellist1')) and in_array("$value->test_name", old('sellist1'))) ? 'selected' : ''}}>{{$value->test_name}}</option>
+                  @endforeach
+                @endif
+                
                 <option value="" selected>None</option>
               </select>
               <div id="error" class="text-warning"></div>
@@ -51,27 +100,14 @@
             
             <div class="form2 form-group col bg-secondary mt-4">
               <label for="file1" class="text-white">Upload a Prescription</label>
-              <input type="file" class="form-control-file border" id="file1" name="file">
+              <input type="file" class="form-control-file border" id="file1" name="file" value="{{old('file')}}">
               @error('file')
               <div class="text-warning">{{$message}}</div>
               @enderror
             </div>
-            <div class="form3 form-group bg-secondary text-white mt-4">
-              <label for="sel2">Select a Lab</label>
-              <select class="form-control" id="sel2" name="sellist2">
-                <option value="vijaya diagnostic centre">Vijaya Diagnostic Centre</option>
-                <option value="neuberg anand reference lab">Neuberg Anand Reference Lab</option>
-                <option value="medplus pathology lab">Medplus Pathology Lab</option>
-                <option value="dr a r diagnostic labs">Dr A R Diagnostic Labs</option>
-                <option value="pathcare labs">Pathcare Labs</option>
-                <option value="" selected>None</option>
-              </select>
-              @error('sellist2')
-              <div class="text-warning">{{$message}}</div>
-              @enderror
-            </div>
+            
             <div class="text-center">
-              <button class="btn bg-secondary text-white" type="submit" name="submit";">Continue</button>
+              <button class="btn bg-secondary text-white" type="submit">Submit</button>
             </div>
           </form>
         </div>
