ce559f31ae75126a6e721c32e106152be3d0268e
Akshay-1314 <66550751+Akshay-1314@users.noreply.github.com>
2020-06-17 23:36:03 +0530

    Add files via upload

---
+++ app/labs_tests.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class labs_tests extends Model
+{
+    //
+    protected $table = 'labs_tests';
+    public $timestamps = false;
+}
